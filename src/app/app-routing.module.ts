656acefab1285a77f661dc418568a0e6d2531e46
shpetimshala <shpetim.sha@gmail.com>
2019-08-06 11:44:15 +0200

    WIP init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {BaseComponent} from './shared/layout/base/base.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: BaseComponent,
+    loadChildren: './shop/shop.module#ShopModule'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/shop/shop.module.ts
+++ src/app/shop/shop.module.ts
@@ -2,12 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ShopRoutingModule } from './shop-routing.module';
+import {SharedModule} from '../shared/shared.module';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { ForgotPasswordComponent } from './auth/forgot-password/forgot-password.component';
+import { NewPasswordComponent } from './auth/new-password/new-password.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [LoginComponent, RegisterComponent, ForgotPasswordComponent, NewPasswordComponent, ProductListComponent],
   imports: [
     CommonModule,
-    ShopRoutingModule
+    ShopRoutingModule,
+    SharedModule
   ]
 })
 export class ShopModule { }
