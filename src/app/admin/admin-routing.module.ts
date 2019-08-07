d369b60f0f40fc09becb71abca6db1cd48798adb
shpetimshala <shpetim.sha@gmail.com>
2019-08-07 10:29:53 +0200

    refactor

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -1,15 +1,14 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
-import {BrandsComponent} from './brands/brands.component';
-import {CategoriesComponent} from './categories/categories.component';
 import {HomeComponent} from './home/home.component';
 import {OrdersComponent} from './orders/orders.component';
 import {UsersComponent} from './users/users.component';
 import {ProductsComponent} from './products/products.component';
-import {RolesComponent} from './roles/roles.component';
+import {RolesComponent} from './users/roles/roles.component';
 import {CustomersComponent} from './customers/customers.component';
 import {DashboardBaseComponent} from './layout/base/dashboard-base.component';
-
+import {PlatformsComponent} from './products/platforms/platforms.component';
+import {PublishersComponent} from './products/publishers/publishers.component';
 
 const routes: Routes = [
   {
@@ -17,11 +16,14 @@ const routes: Routes = [
     component: DashboardBaseComponent,
     children: [
       {path: '', component: HomeComponent},
-      {path: 'categories', component: CategoriesComponent},
       {path: 'customers', component: CustomersComponent},
       {path: 'orders', component: OrdersComponent},
-      {path: 'products', component: ProductsComponent},
-      {path: 'brands', component: BrandsComponent},
+      {
+        path: 'products',
+        component: ProductsComponent
+      },
+      {path: 'products/platforms', component: PlatformsComponent},
+      {path: 'products/publishers', component: PublishersComponent},
       {path: 'users', component: UsersComponent},
       {path: 'roles', component: RolesComponent}
     ]
---
+++ src/app/admin/products/platforms/platforms.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-platforms',
+  templateUrl: './platforms.component.html'
+})
+export class PlatformsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
