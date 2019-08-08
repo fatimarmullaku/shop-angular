22b023af4759c9ade06a2d0f93159e14b38ab337
shpetimshala <shpetim.sha@gmail.com>
2019-08-08 10:41:33 +0200

    view encapsulation fix

--- src/app/shop/products/product-list/product-item/product-item.component.ts
+++ src/app/shop/products/product-list/product-item/product-item.component.ts
@@ -1,7 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-product-item',
+  selector: '[app-product-item]',
   templateUrl: './product-item.component.html',
   styleUrls: ['./product-item.component.scss']
 })
