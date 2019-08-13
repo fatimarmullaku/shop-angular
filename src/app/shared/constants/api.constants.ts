76630923b435db0cdb0ed1805841b8071456dc5f
Fati Marmullaku <fati.marmullaku@gmail.com>
2019-08-13 16:07:51 +0200

    Initialized some constants

---
+++ src/app/shared/constants/api.constants.ts
@@ -0,0 +1,11 @@
+export const DEV_ENV = 'http://157.230.109.179:8080/api/v1';
+export const PROD_ENV = '';
+
+export const ENDPOINT_PREFIX = DEV_ENV;
+
+export const CUSTOMERS_ENDPOINT = ENDPOINT_PREFIX + '/customers';
+export const AUTH_ENDPOINT = ENDPOINT_PREFIX + '/auth'; // TO BE CREATED
+export const PRODUCTS_ENDPOINT = ENDPOINT_PREFIX + '/products'; // TO BE CHANGED
+export const FILE_UPLOAD_ENDPOINT = ENDPOINT_PREFIX + '/upload';
+
+
