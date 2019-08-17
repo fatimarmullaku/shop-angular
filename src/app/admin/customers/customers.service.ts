b2849d09bd5fcdd4f90edb65b8dada4fa958fb21
Enis <enisrasimi@gmail.com>
2019-08-17 09:06:56 +0200

    Customers

--- src/app/admin/customers/customers.service.ts
+++ src/app/admin/customers/customers.service.ts
@@ -1,29 +1,32 @@
-import { Injectable } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-import {ENDPOINTS} from "../../shared/constants/api.constants";
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {ENDPOINTS} from '../../shared/constants/api.constants';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CustomersService {
-readonly getAll = ENDPOINTS.customers.getAll;
-  constructor(private http: HttpClient ) {}
+  readonly rootUrl = 'localhost:3000';
+
+  // readonly getAll = ENDPOINTS.customers.getAll;
+  constructor(private http: HttpClient) {
+  }
 
 
   getAllCustomers() {
-    return this.http.get(this.getAll);
+    return this.http.get(this.rootUrl + '/customers');
   }
 
   registerCustomer(data) {
-    return this.http.post(this.getAll + '/', data);
+    return this.http.post(this.rootUrl + '/', data);
   }
 
   deleteCostumer(id) {
-    return this.http.delete(this.getAll + '/', id);
+    return this.http.delete(this.rootUrl + '/', id);
   }
 
   updateCustomer(data) {
-    return this.http.put( this.getAll + '/', data);
+    return this.http.put(this.rootUrl + '/', data);
   }
 
 
