b36639ebb7dfc5dc33b078bedb35ed4c1786371e
durimgashi <durimgashi97@gmail.com>
2019-08-19 18:43:11 +0200

    Improvements to the home screen, added wishlist and card button and added wishlist component

---
+++ src/app/shop/wishlist/wishlist.component.ts
@@ -0,0 +1,19 @@
+import {Component, OnInit} from '@angular/core';
+import {ProductModel} from "../../shared/models/product.model";
+import {ProductService} from "../../shared/services/product.service";
+
+@Component({
+  selector: 'app-wishlist',
+  templateUrl: './wishlist.component.html'
+})
+export class WishlistComponent implements OnInit {
+
+  items: ProductModel;
+
+
+  constructor(private productService: ProductService) { }
+
+  ngOnInit() {
+  }
+
+}
