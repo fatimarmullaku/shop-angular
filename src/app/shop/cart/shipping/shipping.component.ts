e5b38fb04459d88b339f38ca2c484f42bf47e9ac
Fati Marmullaku <fati.marmullaku@gmail.com>
2019-08-26 15:08:52 +0200

    Fixed bug when loading addresses from server

--- src/app/shop/cart/shipping/shipping.component.ts
+++ src/app/shop/cart/shipping/shipping.component.ts
@@ -1,10 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FormArray, FormBuilder, FormControl, FormGroup} from '@angular/forms';
 import {CustomerModel} from '../../../shared/models/customer.model';
 import {AddressModel} from '../../../shared/models/address.model';
 import {ActivatedRoute, Router} from '@angular/router';
 import {CustomerService} from '../../../shared/services/customer.service';
-import {add} from "ngx-bootstrap/chronos";
+import {HttpRequestMethod} from '../../../shared/constants/http-request.method';
+import {ENDPOINTS} from '../../../shared/constants/api.constants';
+import {RestService} from '../../../shared/services/rest.service';
 
 @Component({
   selector: 'app-shipping',
@@ -20,25 +22,58 @@ export class ShippingComponent implements OnInit {
 
   constructor(private formBuilder: FormBuilder,
               public router: Router,
+              private restService: RestService,
               private activatedRoute: ActivatedRoute,
               private customerService: CustomerService
-  ) { }
+  ) {
+  }
 
   ngOnInit() {
-    this.customer = this.customerService.getCustomer(1);
-
+    this.fetchCustomers();
     this.shippingFormGroup = this.formBuilder.group({
       addresses: this.formBuilder.array([])
     });
+  }
 
-    this.customerAddresses = this.customer.addresses;
+  fetchCustomers(): void {
+    /*const customer1 = new CustomerModel();
+    customer1.id = 1;
+    customer1.firstName = 'John';
+    customer1.lastName = 'Doe';
 
-    const currentAddressFormArray = this.shippingFormGroup.get('addresses') as FormArray;
+    const firstPhoneNumber = new PhoneNumberModel();
+    const secPhoneNumber = new PhoneNumberModel();
 
-    for (const address of this.customerAddresses) {
-      const newAddressGroup = this.createAddress();
-      currentAddressFormArray.push(newAddressGroup);
-    }
+    firstPhoneNumber.home = '+38342655';
+    firstPhoneNumber.mobile = '+24896226';
+    secPhoneNumber.home = '+38344458485';
+    secPhoneNumber.mobile = '+37744258852';
+    customer1.phoneNumbers = [firstPhoneNumber, secPhoneNumber];
+
+    const firstAddress = new AddressModel();
+
+    firstAddress.country = 'Kosovo';
+    firstAddress.city = 'Prishtine';
+    firstAddress.zipCode = 10000;
+    firstAddress.street = 'Bajram Kelmendi';
+
+    // const secAddress = new AddressModel();
+    //
+    // secAddress.country = 'shqiperi';
+    // secAddress.city = 'tirana';
+    // secAddress.zipCode = 10000;
+    // secAddress.street = 'rruga b';
+    customer1.addresses = [firstAddress];
+
+    this.customers = [customer1];*/
+    this.restService.publicRequest<any>(HttpRequestMethod.GET, ENDPOINTS.customers.getAll)
+      .subscribe((res) => {
+          this.customer = res.filter(item => item.id === 1)[0];
+          this.loadAdressesView();
+        },
+        error => {
+          console.error(error);
+        });
   }
 
   createAddress(): FormGroup {
@@ -63,13 +98,23 @@ export class ShippingComponent implements OnInit {
 
   }
 
-  getRadioValue(value: any){
-     if(value == 'radio0'){
-       console.log(this.shippingFormGroup.getRawValue());
-       this.nextClicked = false;
-     }else if(value == 'radio1'){
-       console.log(this.shippingFormGroup.getRawValue());
-       this.nextClicked = false;
-     }
+  getRadioValue(value: any) {
+    if (value === 'radio0') {
+      console.log('VALUE OF RADIO: ', this.shippingFormGroup.getRawValue()[0]);
+      this.nextClicked = false;
+    } else if (value === 'radio1') {
+      console.log('VALUE OF RADIO 2: ', this.shippingFormGroup.getRawValue()[1]);
+      this.nextClicked = false;
+    }
+  }
+
+  private loadAdressesView() {
+    const currentAddressFormArray = this.shippingFormGroup.get('addresses') as FormArray;
+    this.customerAddresses = this.customer.addresses;
+
+    for (const address of this.customerAddresses) {
+      const newAddressGroup = this.createAddress();
+      currentAddressFormArray.push(newAddressGroup);
+    }
   }
 }
