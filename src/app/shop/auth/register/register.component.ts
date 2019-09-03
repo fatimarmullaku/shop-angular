ea53d108839fbd12f73b12268429b4e54ac6b6b2
durimgashi <durimgashi97@gmail.com>
2019-09-03 15:46:39 +0200

    Added buy directly and validated the entire register form

--- src/app/shop/auth/register/register.component.ts
+++ src/app/shop/auth/register/register.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {UserService} from '../../../shared/services/user.service';
 import {UserRegisterModel} from '../../../shared/models/user-register.model';
 import {Router} from '@angular/router';
@@ -42,34 +42,33 @@ export class RegisterComponent implements OnInit {
 
   onRegisterSubmit() {
     this.submitted = true;
-    if (this.registerForm.invalid){
+    if (this.registerForm.invalid) {
       return;
     }
     const payload = new UserRegisterModel();
-      payload.name = this.f.firstName.value + ' ' + this.f.lastName.value;
-      payload.email = this.f.email.value;
-      payload.user = {
-        email: this.f.email.value,
-        password: this.f.password.value
-      };
+    payload.name = this.f.firstName.value + ' ' + this.f.lastName.value;
+    payload.email = this.f.email.value;
+    payload.user = {
+      email: this.f.email.value,
+      password: this.f.password.value
+    };
     const cartStorage = this.baseStorageService.getStorageOf(LocalStorageKey.CART);
     this.userService.register(payload)
-        .subscribe((res) => {
-          this.userService.login(payload.user.email, payload.user.password)
-            .subscribe(r => {
-              this.isRegistered = true;
-              if (cartStorage != null && cartStorage.length > 0) {
-                this.routerLink.navigateByUrl('/auth/additional-information');
-              } else if (cartStorage == null || cartStorage.length == 0) {
-                this.routerLink.navigateByUrl('/auth/additional-information');
-              }
+      .subscribe((res) => {
+        this.userService.login(payload.user.email, payload.user.password)
+          .subscribe(r => {
+            this.isRegistered = true;
+            if (cartStorage != null && cartStorage.length > 0) {
+              this.routerLink.navigateByUrl('/auth/additional-information');
+            } else if (cartStorage == null || cartStorage.length == 0) {
+              this.routerLink.navigateByUrl('/auth/additional-information');
+            }
 
-            }, (err) => {
-              console.error(err);
-            });
-        }, (err) => {
-          alert(err.error.errorMessage);
-        });
-    alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.registerForm.value))
+          }, (err) => {
+            console.error(err);
+          });
+      }, (err) => {
+        alert(err.error.errorMessage);
+      });
   }
 }
