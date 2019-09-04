996e99de7ed4e92d7ce592d5aed53f69a3b95741
Drilon <drilongashi@gmail.com>
2019-09-05 00:28:16 +0200

    search order history added

--- src/app/admin/orders/order-data/order-data.component.ts
+++ src/app/admin/orders/order-data/order-data.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
+import {OrdersService} from '../orders.service';
+import {PaginationService} from '../../../shared/pagination/pagination.service';
+import {OrderModel} from '../orders.model';
 
 @Component({
   selector: 'app-order-data',
@@ -6,12 +9,38 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./order-data.component.scss']
 })
 export class OrderDataComponent implements OnInit {
-  @Input() orderData =  [];
-  constructor() { }
+  searchOrder: string;
+  searchBy = 'customerName';
+
+  data = [];
+  currentPage: number;
+  pageSize = 2;
+  pageSizeOnFilter = 10;
+
+  constructor(private ordersService: OrdersService,
+              private paginationService: PaginationService) { }
 
   ngOnInit() {
+    this.paginationService.changeTotalPages(9);
+    this.paginationService.currentPage.subscribe(currentPage => {
+      this.getOrderHistoryPaged(this.pageSize, currentPage - 1);
+      this.currentPage = currentPage;
+    });
   }
+
+  getOrderHistoryPaged(size: number, page: number) {
+    this.ordersService.paged(size, page).subscribe((orders: OrderModel[]) => {
+      this.data = orders;
+    });
+  }
+
   parseDate(d: string) {
     return new Date(d);
   }
+  searchWithCustomParams() {
+    this.ordersService.pagedWithCustomParams(this.pageSizeOnFilter, this.searchOrder, this.searchBy).subscribe((orders: OrderModel[]) => {
+      this.data = orders;
+    });
+  }
+
 }
