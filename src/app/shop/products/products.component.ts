5a0b0398beb4b857279dc4b9eed0c64c15940d22
Enis <enisrasimi@gmail.com>
2019-09-04 16:31:24 +0200

    Added onFilterChange

--- src/app/shop/products/products.component.ts
+++ src/app/shop/products/products.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {ProductService} from '../../shared/services/product.service';
 import {ProductModel} from '../../shared/models/product.model';
+import {ProductsService} from './products.service';
 
 @Component({
   selector: 'app-products',
@@ -11,12 +12,19 @@ export class ProductsComponent implements OnInit {
 
   products: ProductModel[];
 
-  constructor(private productService: ProductService) {
+  constructor(private productService: ProductService,
+              private productsService: ProductsService) {
   }
 
   ngOnInit() {
     this.products = this.productService.getProducts();
     console.log('from products', this.products);
+  }
+
+  onFilterChange(event: any) {
+    this.productsService.getProductByPlatformAndBrand(event).subscribe(res => {
+      this.products = res;
+    });
   }
 }
 
