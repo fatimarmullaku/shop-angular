d373aa2fff358576abf76b7f5cb6037aa82af6d3
Enis <enisrasimi@gmail.com>
2019-09-05 07:41:41 +0200

    Added filter emitter

--- src/app/shop/products/products.component.ts
+++ src/app/shop/products/products.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ProductService} from '../../shared/services/product.service';
 import {ProductModel} from '../../shared/models/product.model';
+import {ProductsService} from './products.service';
 
 @Component({
   selector: 'app-products',
@@ -9,11 +10,22 @@ import {ProductModel} from '../../shared/models/product.model';
 })
 export class ProductsComponent implements OnInit {
 
-  products: ProductModel[] = [];
+  products: ProductModel[];
 
-  constructor(private productService: ProductService) { }
+  constructor(private productService: ProductService,
+              private productsService: ProductsService) {
+  }
 
   ngOnInit() {
     this.products = this.productService.getProducts();
+    console.log('from products', this.products);
+  }
+
+  onFilterChange(event: any) {
+    this.productsService.getProductByPlatformAndBrand(event).subscribe(res => {
+      this.products = res;
+    });
   }
 }
+
+
