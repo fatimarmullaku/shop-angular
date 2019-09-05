f4eba4d090b73f68da61cde0c617aa491a3e8198
Jehona <jehonaakonushefci@gmail.com>
2019-09-05 09:04:01 +0200

    Added validation for phoneNumber

--- src/app/shop/auth/additional-information/additional-information.component.ts
+++ src/app/shop/auth/additional-information/additional-information.component.ts
@@ -61,4 +61,13 @@ export class AdditionalInformationComponent implements OnInit {
       this.routerLink.navigateByUrl('/');
     }
   }
+
+  numbersOnly(event): boolean {
+    const charCode = (event.which) ? event.which : event.keyCode;
+    if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+      return false;
+    }
+    return true;
+
+  }
 }
