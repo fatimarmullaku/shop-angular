f1d0f867300e79af9219b63d6563c5a91c8bd5ea
Faton <podrimqakuf@gmail.com>
2019-09-07 18:54:24 +0200

    fixed pagination in order history ,made some refactoring,and calls inside pagination component, made pagination dissapear when total pages is 0

--- src/app/admin/orders/order-data/order-data.component.ts
+++ src/app/admin/orders/order-data/order-data.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit, Input } from '@angular/core';
+import {Component, OnInit, Input} from '@angular/core';
 import {OrdersService} from '../orders.service';
 import {PaginationService} from '../../../shared/pagination/pagination.service';
-import {OrderModel} from '../orders.model';
+import {OrderHistoryPagedModel, OrderModel} from '../orders.model';
 
 @Component({
   selector: 'app-order-data',
@@ -9,38 +9,45 @@ import {OrderModel} from '../orders.model';
   styleUrls: ['./order-data.component.scss']
 })
 export class OrderDataComponent implements OnInit {
-  searchOrder: string;
+  searchOrder = '';
   searchBy = 'customerName';
 
-  data = [];
+  data: OrderModel[];
   currentPage: number;
   pageSize = 2;
-  pageSizeOnFilter = 10;
 
   constructor(private ordersService: OrdersService,
-              private paginationService: PaginationService) { }
+              private paginationService: PaginationService) {
+  }
 
   ngOnInit() {
     this.paginationService.changeTotalPages(9);
     this.paginationService.currentPage.subscribe(currentPage => {
-      this.getOrderHistoryPaged(this.pageSize, currentPage - 1);
       this.currentPage = currentPage;
-    });
-  }
-
-  getOrderHistoryPaged(size: number, page: number) {
-    this.ordersService.paged(size, page).subscribe((orders: OrderModel[]) => {
-      this.data = orders;
+      this.searchWithCustomParams();
     });
   }
 
   parseDate(d: string) {
     return new Date(d);
   }
+
   searchWithCustomParams() {
-    this.ordersService.pagedWithCustomParams(this.pageSizeOnFilter, this.searchOrder, this.searchBy).subscribe((orders: OrderModel[]) => {
-      this.data = orders;
-    });
+    this.ordersService.pagedWithCustomParams(this.pageSize, this.currentPage - 1, this.searchOrder, this.searchBy)
+      .subscribe((response: OrderHistoryPagedModel[]) => {
+        if (response.length >= 1) {
+          this.data = response[0].filteredOrdersPage;
+          this.paginationService.changeTotalPages(Math.ceil(response[0].totalFilteredOrders / this.pageSize));
+        } else {
+          this.paginationService.changeTotalPages(0);
+          this.data = [];
+        }
+      });
+  }
+
+  resetPageAndMakeRequest() {
+    this.searchWithCustomParams();
+    this.paginationService.changePage(1);
   }
 
 }
