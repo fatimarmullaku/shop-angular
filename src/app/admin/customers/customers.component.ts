5b04079fd51b9ac32346e288f3b65c54857d1b10
Enis <enisrasimi@gmail.com>
2019-09-10 16:52:10 +0200

    Deleted jpsdf on customers

--- src/app/admin/customers/customers.component.ts
+++ src/app/admin/customers/customers.component.ts
@@ -56,26 +56,6 @@ export class CustomersComponent implements OnInit {
   }
 
 
-<<<<<<< Updated upstream
-
-  public captureScreen() {
-    const data = document.getElementById('contentToConvert');
-    html2canvas(data).then(canvas => {
-      const imgWidth = 210;
-      const pageHeight = 295;
-      const imgHeight = canvas.height * imgWidth / canvas.width;
-      const heightLeft = imgHeight;
-
-      const contentDataURL = canvas.toDataURL('image/png');
-      const pdf = new jspdf('p', 'mm', 'a4'); // A4 size page of PDF
-      const position = 10;
-      pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight);
-      pdf.save('customers.pdf');
-    });
-  }
-=======
->>>>>>> Stashed changes
-
 
   onDelete() {
     this.customersService.deleteCostumer(this.customerId).subscribe(
