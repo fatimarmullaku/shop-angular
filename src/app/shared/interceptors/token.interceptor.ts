9169b4aa38f9148197c36358132b453e00559727
Fati Marmullaku <fati.marmullaku@gmail.com>
2019-09-11 16:55:21 +0200

    WIP init of alert on token expire

--- src/app/shared/interceptors/token.interceptor.ts
+++ src/app/shared/interceptors/token.interceptor.ts
@@ -1,17 +1,31 @@
 import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
 import {Injectable} from '@angular/core';
-import {Observable} from 'rxjs';
+import {Observable, throwError} from 'rxjs';
 import {TokenService} from '../services/token.service';
 import {RestService} from '../services/rest.service';
+import {catchError} from 'rxjs/operators';
+import {UserService} from '../services/user.service';
 
 @Injectable()
 export class TokenInterceptor implements HttpInterceptor {
 
-  constructor(private tokenService: TokenService, private restService: RestService) {
+  constructor(private tokenService: TokenService,
+              private userService: UserService) {
   }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    return next.handle(this.handleHeader(req));
+    return next.handle(this.handleHeader(req)).pipe(
+      catchError(err => {
+        if (err.status === 401) {
+          console.log(err);
+          alert(err.error.message);
+          this.userService.logout();
+          return throwError(err);
+        }
+
+        return throwError(err);
+      })
+    );
   }
 
   private handleHeader(req: HttpRequest<any>) {
@@ -22,15 +36,12 @@ export class TokenInterceptor implements HttpInterceptor {
     } else {
       // TODO: Check if token has time from tokenService method.
 
-      if (this.tokenService.isTokenExpired() === true) {
-        console.log('TOKEN IS EXPIRED');
-      } else {
-        return req.clone({
-          setHeaders: {
-            Authorization: `Bearer ${this.tokenService.getToken()}`
-          }
-        });
-      }
+      return req.clone({
+        setHeaders: {
+          Authorization: `Bearer ${this.tokenService.getToken()}`
+        }
+      });
+
     }
   }
 }
